97282638ffa1066b675701b0b3174bb927755f47
luismonroy1971 <34591655+luismonroy1971@users.noreply.github.com>
2023-06-24 16:58:18 -0500

    Actualizando el usuario con token

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
+import passport from "passport";
 import { signIn, signUp, updateInitial} from "../controllers/user.controller";
 
 const router = Router();
 
 router.post("/signup", signUp);
 router.post("/signin", signIn);
-router.post("/updateinitial/:id", updateInitial);
+router.post("/updateinitial/:id", passport.authenticate("jwt", { session: false }),updateInitial);
 
 export default router;
